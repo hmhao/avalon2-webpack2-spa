3696e2e6d6bf94cf6e14a7e2b5a5d2a6a66d56cb
hmh <215795583@qq.com>
2017-08-01 11:40:28 +0800

    修复IE9- body中script重复拉取的问题

--- src/main.js
+++ src/main.js
@@ -8,6 +8,19 @@ avalon.config({
   local: true
 })
 
+/* 避免avalon在domready时扫描body将script移出移入dom
+ * 导致script重新拉取的问题
+ * IE9及以下才有的问题https://github.com/RubyLouvre/avalon/issues/2040
+ */
+if (avalon.msie && avalon.msie < 10) {
+  let childNodes = document.body.childNodes;
+  for (let i = childNodes.length - 1, node; i >= 0; i--) {
+    node = childNodes[i]
+    if (node.nodeName === 'SCRIPT') {
+      document.body.removeChild(node)
+    }
+  }
+}
 avalon.bootstrap({
   el: '#app',
   component: App
